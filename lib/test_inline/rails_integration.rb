d755f66ea356b868f25ec39ed89e36db586aa46a
Eric Anderson <eric@pixelwareinc.com>
2010-12-09 11:20:06 -0500

    Better support for non-standard directories.
    Provide ActiveSupport::TestCase as default for all non-specified directories so
    lib is no longer a special case and new directories will use that superclass
    by default.

--- lib/test_inline/rails_integration.rb
+++ lib/test_inline/rails_integration.rb
@@ -7,6 +7,12 @@ class Test::Inline::Railtie < Rails::Railtie
   # The paths considered part of the "functional" tests
   cattr_accessor :functional_paths
   self.functional_paths = %w(app/controllers)
+
+  # Anything else uses ActiveSupport::TestCase
+  initializer 'test_inline.register.activesupport_testcase' do
+    Test::Inline.register_abstract_test_case /./, ActiveSupport::TestCase
+  end
+
   # Anything under app/models should use ActiveRecord::TestCase
   initializer 'test_inline.register.activerecord_testcase' do
     Test::Inline.register_abstract_test_case \
@@ -30,7 +36,6 @@ class Test::Inline::Railtie < Rails::Railtie
   # Anything under app/helpers should use ActionView::TestCase
   # Since the TestCase has no name we need to configure helper_class
   initializer 'test_inline.register.actionview_testcase' do
-    require 'action_view/test_case'
     Test::Inline.register_abstract_test_case \
       Regexp.new(Regexp.escape(Rails.root.join('app/helpers'))),
       ActionView::TestCase do |klass, path|
@@ -39,13 +44,6 @@ class Test::Inline::Railtie < Rails::Railtie
       path = path.sub(/\.rb/, '')
       klass.helper_class = path.camelize.constantize
     end
-  end
-
-  # Anything under lib uses ActiveSupport::TestCase
-  initializer 'test_inline.register.activesupport_testcase' do
-    Test::Inline.register_abstract_test_case \
-      Regexp.new(Regexp.escape(Rails.root.join('lib'))),
-      ActiveSupport::TestCase
   end
 
   # We provide tasks for running unit and functional tests (or both)
