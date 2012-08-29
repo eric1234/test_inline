e34abd0a1fecbdcc37b14ea88bd78c2b5a715232
Eric Anderson <eric@pixelwareinc.com>
2012-08-29 09:56:04 -0400

    Removed name randomization.
    When generating test names (required by Test::Unit) we used to generate a
    random name (if one not provided). In case of clashes (common if providing
    a name) it also added a 5 digit number to the end of every name.
    
    Later we found it useful to include the file and line number of the test into
    the name. This made the randomization and the 5 digit number unnecessary.
    This commit removes that making the output much cleaner and reducing the amount
    of code in this library.

--- lib/test_inline/core_ext.rb
+++ lib/test_inline/core_ext.rb
@@ -5,10 +5,8 @@
 # ActiveSupport includes a number of goodies we use. Most people have
 # it due to the proliferation of Rails so rather than duplicating the
 # functionality we will just add the dependency.
-require 'rubygems'
 require 'active_support'
 require 'active_support/core_ext/string/starts_ends_with'
-require 'active_support/core_ext/array/random_access'
 require 'active_support/core_ext/string/inflections'
 
 module Kernel
@@ -18,38 +16,6 @@ module Kernel
       raise StandardError
     rescue
       $!.backtrace[2].split(':', 3)[0..1]
-    end
-  end
-end
-
-class Array
-
-  # In case running under ActiveSupport 2.3.x
-  #
-  # Our def seems to be slightly different than the one from Ruby.
-  # Ruby doesn't repeat the same letter. I actually prefer ours but
-  # for the purpose of this library either will work so if sample
-  # exists then I will defer to that implementation.
-  def sample(n=nil)
-    (0...(n || 1)).inject([]) {|m, i| m << random_element}
-  end unless method_defined? :sample
-
-  # In case running under ActiveSupport < 2.3.8
-  alias_method :random_element, :rand unless method_defined? :random_element
-
-end
-
-class String
-  class << self
-    # Returns a random string of length characters using the given
-    # characterset (default to alphanumeric)
-    def rand length, characters=String.alpha_numeric_charset
-      characters.to_a.sample(length).join
-    end
-
-    # Returns an array of all alphanumeric characters
-    def alpha_numeric_charset
-      ('a'..'z').to_a + ('A'..'Z').to_a + (0..9).to_a
     end
   end
 end
--- test/core_ext_test.rb
+++ test/core_ext_test.rb
@@ -11,19 +11,3 @@ class KernerlTest < Test::Unit::TestCase
   private
   def foo; calling_code end
 end
-
-class StringTest < Test::Unit::TestCase
-  def test_alpha_numeric_charset
-    explicit = %w(
-      a b c d e f g h i j k l m n o p q r s t u v w x y z
-      A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
-    ) + [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-    assert_equal explicit, String.alpha_numeric_charset
-  end
-
-  def test_rand
-    assert_equal 5, String.rand(5).length
-    assert_match /^\w{20}$/, String.rand(20)
-    assert_match /^\d{5}$/, String.rand(5, 0..9)
-  end
-end
